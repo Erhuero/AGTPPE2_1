6741643c9599699e26918dc995efde52d8710811
Constantin CHTANKO <constantin.chtanko@etu.formation-aftec.com>
2019-10-31 09:54:35 +0100

    Première_validation

---
+++ AGTPPE2.1/Controllers/MATERIELsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AGTPPE2._1.Models;
+
+namespace AGTPPE2._1.Controllers
+{
+    public class MATERIELsController : Controller
+    {
+        private BDDKNEntities db = new BDDKNEntities();
+
+        // GET: MATERIELs
+        public ActionResult Index()
+        {
+            var mATERIEL = db.MATERIEL.Include(m => m.TYPECONNEXION1).Include(m => m.UTILISATEUR);
+            return View(mATERIEL.ToList());
+        }
+
+        // GET: MATERIELs/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MATERIEL mATERIEL = db.MATERIEL.Find(id);
+            if (mATERIEL == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mATERIEL);
+        }
+
+        // GET: MATERIELs/Create
+        public ActionResult Create()
+        {
+            ViewBag.typeConnexion = new SelectList(db.TYPECONNEXION, "typeConnexion1", "descriptionConnexion");
+            ViewBag.idUtilisateur = new SelectList(db.UTILISATEUR, "idUtilisateur", "nomUtilisateur");
+            return View();
+        }
+
+        // POST: MATERIELs/Create
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "numeroSerieMateriel,emplacementMateriel,modeleMateriel,ipMateriel,typeIpMateriel,numInfologMateriel,celluleMateriel,etatMateriel,numeroChariot,idUtilisateur,typeConnexion")] MATERIEL mATERIEL)
+        {
+            if (ModelState.IsValid)
+            {
+                db.MATERIEL.Add(mATERIEL);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.typeConnexion = new SelectList(db.TYPECONNEXION, "typeConnexion1", "descriptionConnexion", mATERIEL.typeConnexion);
+            ViewBag.idUtilisateur = new SelectList(db.UTILISATEUR, "idUtilisateur", "nomUtilisateur", mATERIEL.idUtilisateur);
+            return View(mATERIEL);
+        }
+
+        // GET: MATERIELs/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MATERIEL mATERIEL = db.MATERIEL.Find(id);
+            if (mATERIEL == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.typeConnexion = new SelectList(db.TYPECONNEXION, "typeConnexion1", "descriptionConnexion", mATERIEL.typeConnexion);
+            ViewBag.idUtilisateur = new SelectList(db.UTILISATEUR, "idUtilisateur", "nomUtilisateur", mATERIEL.idUtilisateur);
+            return View(mATERIEL);
+        }
+
+        // POST: MATERIELs/Edit/5
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "numeroSerieMateriel,emplacementMateriel,modeleMateriel,ipMateriel,typeIpMateriel,numInfologMateriel,celluleMateriel,etatMateriel,numeroChariot,idUtilisateur,typeConnexion")] MATERIEL mATERIEL)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(mATERIEL).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.typeConnexion = new SelectList(db.TYPECONNEXION, "typeConnexion1", "descriptionConnexion", mATERIEL.typeConnexion);
+            ViewBag.idUtilisateur = new SelectList(db.UTILISATEUR, "idUtilisateur", "nomUtilisateur", mATERIEL.idUtilisateur);
+            return View(mATERIEL);
+        }
+
+        // GET: MATERIELs/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MATERIEL mATERIEL = db.MATERIEL.Find(id);
+            if (mATERIEL == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mATERIEL);
+        }
+
+        // POST: MATERIELs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            MATERIEL mATERIEL = db.MATERIEL.Find(id);
+            db.MATERIEL.Remove(mATERIEL);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ AGTPPE2.1/Controllers/TICKETSController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AGTPPE2._1.Models;
+
+namespace AGTPPE2._1.Controllers
+{
+    public class TICKETSController : Controller
+    {
+        private BDDKNEntities db = new BDDKNEntities();
+
+        // GET: TICKETS
+        public ActionResult Index()
+        {
+            var tICKETS = db.TICKETS.Include(t => t.MATERIEL).Include(t => t.NIVEAUURGENCETICKET).Include(t => t.STATUT).Include(t => t.UTILISATEUR);
+            return View(tICKETS.ToList());
+        }
+
+        // GET: TICKETS/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TICKETS tICKETS = db.TICKETS.Find(id);
+            if (tICKETS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tICKETS);
+        }
+
+        // GET: TICKETS/Create
+        public ActionResult Create()
+        {
+            ViewBag.numeroSerieMateriel = new SelectList(db.MATERIEL, "numeroSerieMateriel", "emplacementMateriel");
+            ViewBag.idUrgence = new SelectList(db.NIVEAUURGENCETICKET, "idUrgence", "libelleUrgence");
+            ViewBag.etatStatut = new SelectList(db.STATUT, "etatStatut", "libelleStatut");
+            ViewBag.idUtilisateur = new SelectList(db.UTILISATEUR, "idUtilisateur", "nomUtilisateur");
+            return View();
+        }
+
+        // POST: TICKETS/Create
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idTicket,emplacementMaterielTicket,typeMaterielTicket,descriptionIncident,dateCreationIncident,dateClotureIncident,idUtilisateur,idUrgence,numeroSerieMateriel,etatStatut")] TICKETS tICKETS)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TICKETS.Add(tICKETS);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.numeroSerieMateriel = new SelectList(db.MATERIEL, "numeroSerieMateriel", "emplacementMateriel", tICKETS.numeroSerieMateriel);
+            ViewBag.idUrgence = new SelectList(db.NIVEAUURGENCETICKET, "idUrgence", "libelleUrgence", tICKETS.idUrgence);
+            ViewBag.etatStatut = new SelectList(db.STATUT, "etatStatut", "libelleStatut", tICKETS.etatStatut);
+            ViewBag.idUtilisateur = new SelectList(db.UTILISATEUR, "idUtilisateur", "nomUtilisateur", tICKETS.idUtilisateur);
+            return View(tICKETS);
+        }
+
+        // GET: TICKETS/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TICKETS tICKETS = db.TICKETS.Find(id);
+            if (tICKETS == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.numeroSerieMateriel = new SelectList(db.MATERIEL, "numeroSerieMateriel", "emplacementMateriel", tICKETS.numeroSerieMateriel);
+            ViewBag.idUrgence = new SelectList(db.NIVEAUURGENCETICKET, "idUrgence", "libelleUrgence", tICKETS.idUrgence);
+            ViewBag.etatStatut = new SelectList(db.STATUT, "etatStatut", "libelleStatut", tICKETS.etatStatut);
+            ViewBag.idUtilisateur = new SelectList(db.UTILISATEUR, "idUtilisateur", "nomUtilisateur", tICKETS.idUtilisateur);
+            return View(tICKETS);
+        }
+
+        // POST: TICKETS/Edit/5
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idTicket,emplacementMaterielTicket,typeMaterielTicket,descriptionIncident,dateCreationIncident,dateClotureIncident,idUtilisateur,idUrgence,numeroSerieMateriel,etatStatut")] TICKETS tICKETS)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tICKETS).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.numeroSerieMateriel = new SelectList(db.MATERIEL, "numeroSerieMateriel", "emplacementMateriel", tICKETS.numeroSerieMateriel);
+            ViewBag.idUrgence = new SelectList(db.NIVEAUURGENCETICKET, "idUrgence", "libelleUrgence", tICKETS.idUrgence);
+            ViewBag.etatStatut = new SelectList(db.STATUT, "etatStatut", "libelleStatut", tICKETS.etatStatut);
+            ViewBag.idUtilisateur = new SelectList(db.UTILISATEUR, "idUtilisateur", "nomUtilisateur", tICKETS.idUtilisateur);
+            return View(tICKETS);
+        }
+
+        // GET: TICKETS/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TICKETS tICKETS = db.TICKETS.Find(id);
+            if (tICKETS == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tICKETS);
+        }
+
+        // POST: TICKETS/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TICKETS tICKETS = db.TICKETS.Find(id);
+            db.TICKETS.Remove(tICKETS);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
